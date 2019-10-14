e790b1ef0d5f5f7b8bcd63f688f891a28ea865e7
aryabhatta <tarun.hive@hotmail.com>
2019-10-15 01:46:23 +0530

    UTC to IST conversion , tested with sunrise

---
+++ User_modules/Sun_IST.js
@@ -0,0 +1,52 @@
+/* ========================= module to convert UTC to IST ========================= */
+
+// Time differences value
+const hrsDiff = 5;
+const minutesDiff = 30;
+const secondsDiff = 00;
+
+//------- Function definitions ----------
+
+exports.USTtoIST = UCTtime => {
+  //slicing the UCT time
+  let hrs = parseInt(UCTtime.slice(0, 2));
+  let minutes = parseInt(UCTtime.slice(3, 5));
+  let seconds = parseInt(UCTtime.slice(6, 8));
+  let meredian = UCTtime.slice(9, 11);
+
+  //converting UCT to IST
+
+  if (seconds + secondsDiff > 60) {
+    minutesDiff++;
+  }
+  seconds = (seconds + secondsDiff) % 60;
+
+  if (minutes + minutesDiff > 60) {
+    hrs++;
+  }
+  minutes = (minutes + minutesDiff) % 60;
+
+  if (hrs < 12 && hrs + hrsDiff > 12) {
+    if (meredian === "AM") meredian = "PM";
+    else meredian = "AM";
+  }
+  hrs = (hrs + hrsDiff) % 12;
+  return standardTimeFormat(hrs, minutes, seconds, meredian);
+};
+
+standardTimeFormat = (hrs, minutes, seconds, meredian) => {
+  //converting int to String type
+
+  hrs = hrs.toString();
+  minutes = minutes.toString();
+  seconds = seconds.toString();
+
+  if (hrs.length === 1) hrs = 0 + hrs;
+  if (minutes.length === 1) minutes = 0 + minutes;
+  if (seconds.length === 1) seconds = 0 + seconds;
+
+  return `${hrs}:${minutes}:${seconds} ${meredian}`;
+};
+
+//calling test function
+// USTtoIST("11:12:00 PM");
--- script.js
+++ script.js
@@ -6,18 +6,20 @@
  * *************************************************/
 
 //latitude & longitudes of Delhi
-const latitude = 28.7041;
-const longitude = 77.1025;
+const latitude = 28.6517178;
+const longitude = 77.2219388;
 const curdate = new Date().toISOString().slice(0, 10);
 
 const request = require("request");
 const url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=${curdate}`;
+const Sun_IST = require("./User_modules/Sun_IST");
 
 request(url, (err, res, body) => {
   if (err) console.log(err);
   else {
     const data = JSON.parse(body);
-    console.log(data.results.sunrise);
+    console.log("Sunrise (UTC) : " + data.results.sunrise);
+    console.log("Sunrise (IST) : " + Sun_IST.USTtoIST(data.results.sunrise));
   }
 });
 
