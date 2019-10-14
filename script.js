80b0a4d4442f1a85ef0af494551a64d27411e062
aryabhatta <tarun.hive@hotmail.com>
2019-10-14 21:50:57 +0530

    Intial commits

---
+++ script.js
@@ -0,0 +1,6 @@
+/**************************************************
+ * Project : UCV
+ * Version : 1.0.0
+ * Description : RTC based movement of solar panel
+ * Start Date : 14 Oct, 2019
+ * *************************************************/
