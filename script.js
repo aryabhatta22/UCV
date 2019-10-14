1b9da2cf3914a37065b2e177443c7fdb34e9821b
aryabhatta <tarun.hive@hotmail.com>
2019-10-14 22:52:37 +0530

    data sending to aurdino tested Incomplete

--- script.js
+++ script.js
@@ -4,3 +4,14 @@
  * Description : RTC based movement of solar panel
  * Start Date : 14 Oct, 2019
  * *************************************************/
+
+const SerialPort = require("serialport");
+const port = new SerialPort("COM5", {
+  baudRate: 9600
+});
+
+port.write("Testing message");
+// port.write1(Buffer.from("Testing message"));
+SerialPort.list((err, ports) => {
+  console.log(ports[0].comName); //portName
+});
