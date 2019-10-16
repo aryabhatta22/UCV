1e359a07f18185e0d43bc1a1d8662e4001fedf97
aryabhatta <tarun.hive@hotmail.com>
2019-10-16 16:13:40 +0530

    communication btw aurdino and nodejs tested

--- script.js
+++ script.js
@@ -6,9 +6,10 @@
  * *************************************************/
 
 //latitude & longitudes of Delhi
-const latitude = 28.6517178;
-const longitude = 77.2219388;
+const latitude = 28.7041;
+const longitude = 77.1025;
 const curdate = new Date().toISOString().slice(0, 10);
+console.log("Current Date : " + curdate);
 
 const request = require("request");
 const url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=${curdate}`;
@@ -25,13 +26,42 @@ request(url, (err, res, body) => {
 
 // FIXME:
 
-// const SerialPort = require("serialport");
-// const port = new SerialPort("COM5", {
-//   baudRate: 9600
-// });
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
+const port = new SerialPort(
+  "COM5",
+  {
+    baudRate: 9600
+  },
+  false
+);
+const parser = port.pipe(new Readline({ delimiter: "\n" }));
 
-// port.write("Testing message");
-// // port.write1(Buffer.from("Testing message"));
-// SerialPort.list((err, ports) => {
-//   console.log(ports[0].comName); //portName
-// });
+port.on("open", () => {
+  console.log("serial port open");
+
+  setTimeout(() => {
+    port.write("Hello Ashu\n", err => {
+      if (err) console.log("Error has occured" + err);
+    });
+  }, 5000);
+
+  setTimeout(() => {
+    port.write("Im aurdino UNO\n", err => {
+      if (err) console.log("Error has occured" + err);
+    });
+  }, 5000);
+
+  parser.on("data", data => {
+    console.log("Aurdino : ", data.toString());
+  });
+
+  //   port.close(function() {
+  //     console.log("port Closed.");
+  //   });
+});
+
+// port.write1(Buffer.from("Testing message"));
+SerialPort.list((err, ports) => {
+  console.log(ports[0].comName); //portName
+});
