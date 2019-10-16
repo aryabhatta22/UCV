24052716482cb6b91478144720699d9c7eeb688f
aryabhatta <tarun.hive@hotmail.com>
2019-10-17 02:41:35 +0530

    DayLength and delay provided to aurdino

--- script.js
+++ script.js
@@ -5,26 +5,53 @@
  * Start Date : 14 Oct, 2019
  * *************************************************/
 
+// ------------------- Data fetch and manipulation -------------------
+
 //latitude & longitudes of Delhi
 const latitude = 28.7041;
 const longitude = 77.1025;
 const curdate = new Date().toISOString().slice(0, 10);
+const nextDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+  .toISOString()
+  .slice(0, 10);
 console.log("Current Date : " + curdate);
+console.log("Next Date : " + nextDate);
 
 const request = require("request");
-const url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=${curdate}`;
+const today = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=${curdate}`;
+const tommorow = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=${nextDate}`;
 const Sun_IST = require("./User_modules/Sun_IST");
+const HandlePanel = require("./User_modules/HandlePanel");
 
-request(url, (err, res, body) => {
+let dayLength = "";
+let Sunset = "";
+let SunriseTmrw = "";
+request(today, (err, res, body) => {
   if (err) console.log(err);
   else {
     const data = JSON.parse(body);
-    console.log("Sunrise (UTC) : " + data.results.sunrise);
-    console.log("Sunrise (IST) : " + Sun_IST.USTtoIST(data.results.sunrise));
+    dayLength = data.results.day_length;
+    Sunset = Sun_IST.UCTtoIST(data.results.sunset);
+    console.log("\n======== Todays's Sun Info ========\n");
+    console.log("Sunrise (IST) : " + Sun_IST.UCTtoIST(data.results.sunrise));
+    console.log("Sunset (IST) : " + Sun_IST.UCTtoIST(data.results.sunset));
+    console.log("Day Length (IST) : " + data.results.day_length);
   }
 });
 
-// FIXME:
+request(tommorow, (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    const data = JSON.parse(body);
+    SunriseTmrw = Sun_IST.UCTtoIST(data.results.sunrise);
+    console.log("\n======== Tommorow's Sun Info ========\n");
+    console.log("Sunrise (IST) : " + Sun_IST.UCTtoIST(data.results.sunrise));
+    console.log("Sunset (IST) : " + Sun_IST.UCTtoIST(data.results.sunset));
+    console.log("Day Length (IST) : " + data.results.day_length);
+  }
+});
+
+// ------------------- Serial communication with aurdino -------------------
 
 const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
@@ -38,16 +65,22 @@ const port = new SerialPort(
 const parser = port.pipe(new Readline({ delimiter: "\n" }));
 
 port.on("open", () => {
-  console.log("serial port open");
+  console.log("***serial port open***");
 
   setTimeout(() => {
-    port.write("Hello Ashu\n", err => {
-      if (err) console.log("Error has occured" + err);
-    });
+    port.write(
+      `${HandlePanel.workingSpan(dayLength)}\n${HandlePanel.delay(
+        Sunset,
+        SunriseTmrw
+      )}\n`,
+      err => {
+        if (err) console.log("Error has occured" + err);
+      }
+    );
   }, 5000);
 
   setTimeout(() => {
-    port.write("Im aurdino UNO\n", err => {
+    port.write("String ended\n", err => {
       if (err) console.log("Error has occured" + err);
     });
   }, 5000);
@@ -55,13 +88,8 @@ port.on("open", () => {
   parser.on("data", data => {
     console.log("Aurdino : ", data.toString());
   });
-
-  //   port.close(function() {
-  //     console.log("port Closed.");
-  //   });
 });
 
-// port.write1(Buffer.from("Testing message"));
 SerialPort.list((err, ports) => {
-  console.log(ports[0].comName); //portName
+  console.log("Working port : " + ports[0].comName); //portName
 });
